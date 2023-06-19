0622b24c7bd9e2b2602df7ed89bc0c5d1d4860f5
Abaylooper <abaybrand13@gmail.com>
2023-06-19 15:32:15 -0700

    this issome change

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,9 +19,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
 
-	if (lastd > 5)
-	{
-	printf("last digit of %d is %D and is greater than 5\n", n, lastd);
+if (lastd > 5)
+{
+printf("last digit of %d is %d and is greater than 5\n", n, lastd);
 }
 else if (lastd == 0)
 {
