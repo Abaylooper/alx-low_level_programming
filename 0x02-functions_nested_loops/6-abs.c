add1ca02f9c9b5c2fff9b9a9d3065c0b08e64959
Abaylooper <abaybrand13@gmail.com>
2023-07-01 14:39:48 -0700

    this 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - returns the absolut value of number
+ * @n: the number we find the abs value
+ * Return: the abasolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
