b7267a52f84fcb274e2a149d52b2e642d0319731
Abaylooper <abaybrand13@gmail.com>
2023-07-02 04:21:08 -0700

    the seventh task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function prints the last digit of the number
+ * @n: is the number to printed it is last digit
+ *
+ * Return: "num" after execution
+ */
+
+int print_last_digit(int n)
+
+{
+	int num;
+
+	num = n % 10;
+
+	return (num);
+
+}
