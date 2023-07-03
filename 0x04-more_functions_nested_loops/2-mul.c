a14e68a2b8067253840afbcc17ee54c7d13ee690
Abaylooper <abaybrand13@gmail.com>
2023-07-03 07:51:28 -0700

    this is the second task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul - This function multiplies two integers
+ * @a: is the first number
+ * @b: is the secind number
+ * Return: the final result
+ */
+
+int mul(int a, int b)
+
+{
+	int num;
+
+	num = a * b;
+
+	return (num);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,5 +10,7 @@ int _putchar(char c);
 
 int _isdigit(int c);
 
+int mul(int a, int b);
+
 
 #endif /*MAIN_H*/
