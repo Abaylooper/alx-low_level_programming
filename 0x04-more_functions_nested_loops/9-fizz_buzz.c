eb7b3a7031856ccecb33ef905368285c42ff2bb2
Abaylooper <abaybrand13@gmail.com>
2023-07-03 14:13:27 -0700

    this is the ninth task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100 with fizz buzz
+ * Return: entry point
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
