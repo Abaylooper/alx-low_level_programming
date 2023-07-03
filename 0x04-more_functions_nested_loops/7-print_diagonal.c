a461f33831875546640b79bfcf67c28df4fdbaa4
Abaylooper <abaybrand13@gmail.com>
2023-07-03 13:29:53 -0700

    some modification

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,11 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n >= 0)
+	if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 1; j < i; j++)
+			for (j = 2; j <= i; j++)
 			{
 				_putchar(' ');
 			}
@@ -23,5 +23,6 @@ void print_diagonal(int n)
 
 		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
