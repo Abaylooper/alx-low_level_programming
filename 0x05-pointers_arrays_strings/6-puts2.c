47ff0e34597232ba82d6e9ce6e69b8fae22f53b5
Abaylooper <abaybrand13@gmail.com>
2023-07-07 02:26:23 -0700

    this is the siz task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - used to print every other character
+ * @str: is the string to be printed
+ * Return: the value of the code
+ */
+
+void puts2(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
