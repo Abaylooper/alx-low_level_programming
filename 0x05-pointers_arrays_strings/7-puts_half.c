2059c96227f02aab2072c5d99ede998fbd5dd82d
Abaylooper <abaybrand13@gmail.com>
2023-07-07 03:56:18 -0700

    this is the some modification

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,18 @@ void puts_half(char *str)
 {
 	int j = 0;
 	int i;
+	int len;
 
 	while (str[j] != '\0')
 	{
 		j++;
 	}
+	if (j % 2 == 0)
+		len = j / 2;
+	else
+		len = (j - 1) / 2;
 
-	for (i = j / 2; i < j; i++)
+	for (i = len; i < j; i++)
 	{
 		_putchar(str[i]);
 	}
