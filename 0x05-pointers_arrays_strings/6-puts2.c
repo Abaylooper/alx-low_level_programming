cd9a2ba011a077ea041245e9a5382f237c9781ca
Abaylooper <abaybrand13@gmail.com>
2023-07-07 03:24:12 -0700

    some change

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,18 @@ void puts2(char *str)
 
 {
 	int i;
+	int j = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
 
-		if (str[i] == '.')
-			break;
 
 	}
 	_putchar('\n');
