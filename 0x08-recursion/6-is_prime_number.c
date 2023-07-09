d1428d47323d61601842471c0a06aadcac04ec2b
Abaylooper <abaybrand13@gmail.com>
2023-07-09 05:21:39 -0700

    this is some modification

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,9 @@ int prime_helper(long int n, long int i);
 int is_prime_number(int n)
 
 {
-	if (n == 0 || n == 1 || n == -1)
+	if (n < 0)
+		return (0);
+	if (n == 0 || n == 1)
 		return (0);
 	return (prime_helper(n, n));
 }
